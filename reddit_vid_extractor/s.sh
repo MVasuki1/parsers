086da26120a969f51305e6cdc8868da8c4302717
M Vasuki <129155240+MVasuki1@users.noreply.github.com>
2024-03-21 18:35:03 +0530

    Fix reddit video extractor script

--- reddit_vid_extractor/s.sh
+++ reddit_vid_extractor/s.sh
@@ -1,1 +1,1 @@
-curl -s https://old.reddit.com/r/javdreams/.json | jq -r '.data.children[]| .data.preview.reddit_video_preview.fallback_url' out.json | grep -v '^$' | grep -v null
+curl -s https://old.reddit.com/r/javdreams/.json | jq -r '.data.children[]| .data.preview.reddit_video_preview.fallback_url' | grep -v '^$' | grep -v null
